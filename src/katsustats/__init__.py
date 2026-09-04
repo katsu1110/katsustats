5efda78da6f3e2e9340ac9e0e87877f5f286a53f
katsu1110 <code1110g-show@hotmail.co.jp>
2026-09-04 22:35:48 +0900

    chore: bump version to 0.12.0

--- src/katsustats/__init__.py
+++ src/katsustats/__init__.py
@@ -16,6 +16,6 @@ from __future__ import annotations
 
 from . import plots, reports, stats, utils  # noqa: F401
 
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 
 __all__ = ["plots", "reports", "stats", "utils"]
